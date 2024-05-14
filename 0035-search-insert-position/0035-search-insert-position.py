a70cc89fc55106df4062c266e9817d70b264aa9f
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-05-14 20:44:14 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low=0
+        right=len(nums)-1
+        while low<right:
+            mid=(low+right)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                low=mid+1
+            else:
+                right=mid-1
+        return right +1      
