4d06de54a042413ee29cbae266cab4b77a206d94
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-07-02 20:59:03 +0300

    Time: 482 ms (40.81%), Space: 19.5 MB (67.52%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,28 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        q = deque()
+        ans = [[float('inf')] * n for _ in range(m)]
+
+        def valid(x, y):
+            return 0 <= x < m and 0 <= y < n
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    q.append((i, j))
+                    ans[i][j] = 0
+
+        while q:
+            i, j = q.popleft()
+            for dx, dy in directions:
+                x, y = i + dx, j + dy
+                if valid(x, y) and ans[x][y] > ans[i][j] + 1:
+                    ans[x][y] = ans[i][j] + 1
+                    q.append((x, y))
+
+        return ans
