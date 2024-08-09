baf472dc0e9a994fb732630c0a66bb4741cc0b5d
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-08-09 18:01:47 +0300

    Time: 33 ms (67.91%), Space: 16.6 MB (14.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,21 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n==1:
-            return 1
-        elif n==2:
-            return 2
-        else:
-            return self.climbStairs(n-1)+self.climbStairs(n-2)        
+        memo = {}
+        
+        def solve(level):
+            if level > n:
+                return 0
+            if level == n:
+                return 1
+            if level in memo:
+                return memo[level]
+            
+            ans = 0
+            for i in range(1, 3):
+                ways = solve(level + i)
+                ans += ways
+            
+            memo[level] = ans
+            return ans
+        
+        return solve(0)
