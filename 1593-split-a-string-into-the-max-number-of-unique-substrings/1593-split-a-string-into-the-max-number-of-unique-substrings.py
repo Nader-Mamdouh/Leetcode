d412d1fb36ec1e0b0887c070b8e11c76f4958947
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-10-21 12:52:02 +0300

    Time: 147 ms (92.38%), Space: 16.7 MB (19.96%) - LeetHub

---
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxUniqueSplit(self, s: str) -> int:
+        
+        def back(i,cur_set):
+            if i ==len(s):
+                return 0
+            res=0    
+            for j in range(i,len(s)):
+                sub=s[i:j+1]
+                if sub in cur_set:
+                    continue
+                cur_set.add(sub)
+                res=max(res,1+back(j+1,cur_set))
+                cur_set.remove(sub)
+            return res
+        return back(0,set())                
