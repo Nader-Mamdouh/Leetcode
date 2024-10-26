37277f698b5c1cdc6753841cd927bab79dc4ab1c
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-10-26 16:26:08 +0300

    Time: 563 ms (93.37%), Space: 84.2 MB (64.72%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,30 +1,55 @@
 def manhattan_distance(p1: List[int], p2: List[int]) -> int:
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0 for _ in range(n)]
+        
+    def find(self, u):
+        if self.parent[u] == u:
+            return u
+        self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+    
+    def union(self, u, v):
+        u = self.find(u)
+        v = self.find(v)
+        
+        if u == v:
+            return False
+        
+        if self.rank[u] > self.rank[v]:
+            u, v = v, u
+            
+        self.parent[u] = v
+        
+        if self.rank[u] == self.rank[v]:
+            self.rank[v] += 1
+        
+        return True
+
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-        visited = [False] * n
-        heap_dict = {0: 0}  
-        min_heap = [(0, 0)]
+        uf = UnionFind(n)
+        
+        edges = []
+        
+        for i in range(n):
+            for j in range(i+1, n):
+                distance = manhattan_distance(points[i], points[j])
+                heappush(edges, (distance, i, j))
         
         mst_weight = 0
+        mst_edges = 0
         
-        while min_heap:
-            w, u = heappop(min_heap)
-            
-            if visited[u] or heap_dict.get(u, float('inf')) < w:
-                continue
-            
-            visited[u] = True
-            mst_weight += w
-            
-            for v in range(n):
-                if not visited[v]:
-                    new_distance = manhattan_distance(points[u], points[v])
-      
-                    if new_distance < heap_dict.get(v, float('inf')):
-                        heap_dict[v] = new_distance
-                        heappush(min_heap, (new_distance, v))
-        
+        while edges:
+            w, u, v = heappop(edges)
+            if uf.union(u, v):
+                mst_weight += w
+                mst_edges += 1
+                if mst_edges == n - 1:
+                    break
+                    
         return mst_weight
