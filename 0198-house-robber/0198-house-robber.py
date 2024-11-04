3ada29bcdf02c197050ebb88f9fc1516a9effa77
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-11-04 13:59:28 +0200

    Time: 0 ms (100%), Space: 16.5 MB (52.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n=len(nums)
-        memo={}
-        def solve(index):
-            if index>=n:
-                return 0
-            if index in memo:
-                return memo[index]    
-            leng=0
-            leng=max(leng,nums[index]+solve(index+2) )
-            memo[index]=leng
-            return leng  
-        mx=-1
-        for i in range(n):
-            mx=max(mx,solve(i) )
-        return mx            
+        dp=[0]*(len(nums))
+        if len(dp)==1:
+            return nums[0]
+        if len(dp)==2:
+            return max(nums)
+        dp[0]=nums[0]
+        dp[1]=max(nums[1],nums[0])       
+        for i in range(2,len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return max(dp)    
+                
