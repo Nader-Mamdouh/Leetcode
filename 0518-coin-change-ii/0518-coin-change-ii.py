1ad3212f8a0acd7c00ea5e0853c002766eb82b34
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-11-06 23:07:55 +0200

    Time: 119 ms (95.01%), Space: 16.8 MB (81.85%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,17 +1,11 @@
-from typing import List, Dict, Tuple
+from typing import List
+
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        memo: Dict[Tuple[int, int], int] = {}       
-        def dfs(index: int, cur_amount: int) -> int:
-            if cur_amount == amount:
-                return 1
-            if cur_amount > amount or index == len(coins):
-                return 0
-            if (index, cur_amount) in memo:
-                return memo[(index, cur_amount)]
-            include = dfs(index, cur_amount + coins[index])
-            exclude = dfs(index + 1, cur_amount)
-            memo[(index, cur_amount)] = include + exclude
-            return memo[(index, cur_amount)]
+        dp = [0] * (amount + 1)
+        dp[0] = 1  
+        for coin in coins:
+            for cur_amount in range(coin, amount + 1):
+                dp[cur_amount] += dp[cur_amount - coin]
         
-        return dfs(0, 0)
+        return dp[amount]
