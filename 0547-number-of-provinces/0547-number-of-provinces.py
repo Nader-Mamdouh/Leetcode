8c51fb8883f7c95098d16ac9189883c83c35b278
Nader Mamdouh <nadermamdouh20@gmail.com>
2024-11-13 16:02:25 +0200

    Time: 8 ms (37.84%), Space: 17.7 MB (50.86%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,31 +1,31 @@
+from typing import List
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        parents = [i for i in range(1001)]
-        size = [1 for i in range(1001)]
-        n=len(isConnected)
-        m=len(isConnected[0])
-        ans=0
-        def union(node1,node2):
-            p1,p2 = find(node1),find(node2)
+        n = len(isConnected)
+        parent = [i for i in range(n)]
+        rank = [1] * n
 
-            if p1 == p2:
-                return False
+        def find(city):
+            if city != parent[city]:
+                parent[city] = find(parent[city])  
+            return parent[city]
 
-            if size[p1] > size[p2]:
-                parents[p2] = p1
-                size[p1] += size[p2]
-            else:
-                parents[p1] = p2
-                size[p2] += size[p1]
-                
-            return True
-        def find(node):
-            if node == parents[node]:
-                return node
-            return find(parents[node])
+        def union(city1, city2):
+            root1 = find(city1)
+            root2 = find(city2)
+
+            if root1 != root2:
+                if rank[root1] > rank[root2]:
+                    parent[root2] = root1
+                elif rank[root1] < rank[root2]:
+                    parent[root1] = root2
+                else:
+                    parent[root2] = root1
+                    rank[root1] += 1
 
         for i in range(n):
-            for j in range(i+1,m):
+            for j in range(i + 1, n):
                 if isConnected[i][j] == 1:
                     union(i, j)
-        return len(set(find(i) for i in range(n)))           
+        return len(set(find(i) for i in range(n)))
