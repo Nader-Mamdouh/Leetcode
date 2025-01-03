b381abbb17f6926ad6928f4aa99c8d4894cfa89b
Nader Mamdouh <nadermamdouh20@gmail.com>
2025-01-03 17:02:37 +0200

    Time: 43 ms (27.5%), Space: 55.2 MB (7.5%) - LeetHub

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.py
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.py
@@ -5,19 +5,20 @@ class Solution(object):
         :type queries: List[List[int]]
         :rtype: List[int]
         """
-        anss=[]
+        vowels = {'a', 'e', 'i', 'o', 'u'}
         prefix=[0]
-        for i in range(len(s)):
-            if s[i][0]=='a' or s[i][0]=='e' or s[i][0]=='i' or s[i][0]=='o' or s[i][0]=='u' and s[i][-1]=='a' or s[i][-1]=='e' or s[i][-1]=='i' or s[i][-1]=='o' or s[i][-1]=='u':
+        anss=[]
+        for word in s:
+            if word[0] in vowels and word[-1] in vowels:
                 prefix.append(1)
             else:
                 prefix.append(0)
-        for i in range (1,len(prefix)) :
-            prefix[i]+=prefix[i-1]
-        #print(prefix)
+
+        for i in range(1, len(prefix)):
+            prefix[i] += prefix[i - 1]
+
         for q in range(len(queries)):
-            ans=prefix[queries[q][1]+1]-prefix[queries[q][0]]
+            ans = prefix[queries[q][1] + 1] - prefix[queries[q][0]]
             anss.append(ans)
-        return anss    
-           
+        return(anss)
         
